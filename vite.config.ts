c6d3e76490d78278f087de3ae05bab8e204fa7da
data4you01-spec <data4you01@gmail.com>
2026-04-17 02:00:37 +0530

    fix: update base url for github pages to match repo name string

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ipl-2026-pro/',
+  base: '/ipl-2026-pro1/',
 })
